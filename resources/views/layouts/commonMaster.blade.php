208bf4e7f03efc4ae6a347c0e75a4b54957920de
lutfifuadi <luthfifuadimajid@gmail.com>
2026-06-22 21:31:56 +0700

    fix(pwa): add mobile-web-app-capable meta tag for non-Apple devices

--- resources/views/layouts/commonMaster.blade.php
+++ resources/views/layouts/commonMaster.blade.php
@@ -62,6 +62,7 @@
   <link rel="manifest" href="{{ asset('manifest.json') }}">
   <meta name="theme-color" content="#7367f0">
   <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
   <meta name="apple-mobile-web-app-title" content="Pelatihanku">
   <link rel="apple-touch-icon" href="{{ asset('icons/icon-192x192.png') }}">
